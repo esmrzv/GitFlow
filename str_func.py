4a1cd57e49173ef7842ed93d19c25761eab52238
esmrzv <esmurziev.1999@mail.ru>
2024-02-19 15:08:30 +0300

    новая функция с докстрингами к 1 и 2 функции

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def uppercase_string(input_string):
+    """Эта ффункция принимает строку и возвращает
+    строку с заглавными буквами"""
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+
+    """Эта функция принимает строку input_string
+     и использует метод capitalize() для преобразования
+      первой буквы каждого слова в строке в заглавную."""
+
+    return ' '.join(word.capitalize() for word in input_string.split())
